f9e07c0f44227e6d57b5c5883a704ef2edf13887
Javier Vivallos <jvivallos@gmail.com>
2025-10-26 19:58:02 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    command = input()
+    print("{}: command not found".format(command))
     pass
 
 
