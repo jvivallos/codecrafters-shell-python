7c04a257a01ee8b82bc94897e3b51902ee6c8ac8
Javier Vivallos <jvivallos@gmail.com>
2025-10-26 21:02:20 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,11 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+
+        if(command.startswith("exit 0")):
+            return 0
         print("{}: command not found".format(command))
-        pass
+        
 
 
 if __name__ == "__main__":
