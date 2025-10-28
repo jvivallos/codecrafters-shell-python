b92d4d45d153d00d600e409ed8a6c86869b24c06
Javier Vivallos <jvivallos@gmail.com>
2025-10-27 21:45:17 -0300

    codecrafters submit [skip ci]

--- app/builtin_command.py
+++ app/builtin_command.py
@@ -1,16 +1,25 @@
+import shutil
+
 class BuiltinCommand:
     def __init__(self, command:str, parameters:str):
         self.command = command
         self.parameters = parameters
 
+    def _type(self):
+        if BuiltinCommand.is_builtin(self.parameters):
+            print(f"{ self.parameters } is a shell builtin")
+        else:
+            location = shutil.which(self.parameters)
+            if location is not None:
+                print(f"{self.parameters} is {location}")
+            else:
+                print(f"{self.parameters}: not found")
+
     def execute(self):
         if self.command == "echo":
             print(self.parameters)
         elif self.command == "type":
-            if BuiltinCommand.is_builtin(self.parameters):
-                print(f"{ self.parameters } is a shell builtin")
-            else:
-                print(f"{ self.parameters } not found")
+            self._type()
 
     @staticmethod 
     def is_builtin(command):
